7c731ed99dfc190703c57837cd5a5a9148c812fe
Aman Gautam <129865249+amangautamdev@users.noreply.github.com>
2024-04-14 21:12:02 +0530

    Add files via upload

---
+++ plugins/route.py
@@ -0,0 +1,26 @@
+# amangautamdev
+# Don't Remove Credit 🥺
+# Telegram Channel @LegendUnion
+# Backup Channel @LegendDawnload
+# Developer @LegendRobot
+
+
+
+
+
+from aiohttp import web
+
+routes = web.RouteTableDef()
+
+@routes.get("/", allow_head=True)
+async def root_route_handler(request):
+    return web.json_response("LegendBotzs")
+
+
+
+
+# amangautamdev
+# Don't Remove Credit 🥺
+# Telegram Channel @LegendUnion
+# Backup Channel @LegendDawnload
+# Developer @LegendRobot
